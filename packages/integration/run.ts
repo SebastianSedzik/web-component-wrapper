c23e69c2b50fdd23395309d2d1e1e6ee821f6987
Sebastian Sędzik <s.sedzik@smartrecruiters.com>
2023-11-02 18:13:45 +0100

    chore(web-component-wrapper-angular): support for `componentImport` & `componentTag` options

--- packages/integration/run.ts
+++ packages/integration/run.ts
@@ -4,9 +4,10 @@ import { processProject } from "web-component-wrapper-core";
 processProject({
   // src: '../../examples/lit-ts/src/spl-button.ts',
   src: '/Users/sebastiansedzik/Projects/filament-product-language/packages/core-components/src/lib/components/avatar/src/spl-avatar.ts',
+  // src: '/Users/sebastiansedzik/Projects/filament-product-language/packages/core-components/src/lib/components/**/*.ts',
   dist: 'dist',
   generator: new AngularComponentsGenerator({
-  
+    componentImport: ({className}) => `import {${className}} from '@sr/spl-core-components/dist';`,
   }),
   typescript: {
     project: '/Users/sebastiansedzik/Projects/filament-product-language/packages/core-components/tsconfig.lib.json',
