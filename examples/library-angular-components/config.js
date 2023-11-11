4ac76938676c62794f2360900c546497a8d36c18
Sebastian Sędzik <s.sedzik@smartrecruiters.com>
2023-11-11 17:58:35 +0100

    chore(examples): support css imports

--- examples/library-angular-components/config.js
+++ examples/library-angular-components/config.js
@@ -2,10 +2,11 @@ const { processProject } = require('web-component-wrapper-core');
 const { AngularComponentsGenerator } = require('web-component-wrapper-angular');
 
 processProject({
-  src: '../library/src/*.ts',
+  src: '../library/src/tag-lit-js.js',
   dist: './dist',
   typescript: {
-    project: '../library/tsconfig.json'
+    project: '../library/tsconfig.json',
+    includes: ['../library/src/vite-env.d.ts']
   },
   generator: new AngularComponentsGenerator({
     webComponentImportPath: () => 'lit-ts-components', // @todo
--- examples/library/src/vite-env.d.ts
+++ examples/library/src/vite-env.d.ts
@@ -1,1 +1,4 @@
 /// <reference types="vite/client" />
+
+// web-component-wrapper-core requires that file exports something :(
+export {}
