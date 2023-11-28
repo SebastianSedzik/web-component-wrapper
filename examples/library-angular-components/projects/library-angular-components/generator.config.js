1180a963d28ab76cdc567dce31b952881d9f3534
Sebastian Sędzik <s.sedzik@smartrecruiters.com>
2023-11-28 21:00:58 +0100

    chore(examples): improve angular library example

--- examples/library-angular-components/projects/library-angular-components/generator.config.js
+++ examples/library-angular-components/projects/library-angular-components/generator.config.js
@@ -2,11 +2,11 @@ const { processProject } = require('@web-component-wrapper/core');
 const { AngularComponentsGenerator } = require('@web-component-wrapper/angular');
 
 processProject({
-  src: '../library/src/*.(ts|js)',
-  dist: './dist',
+  src: '../../../library/src/*.(ts|js)', // path to `library` source files
+  dist: './src/lib/generated-components', // path where wrappers should be generated, in our case we want to put them to `src`
   typescript: {
-    project: '../library/tsconfig.json',
-    includes: ['../library/src/vite-env.d.ts']
+    project: '../../../library/tsconfig.json',
+    includes: ['../../../library/src/vite-env.d.ts']
   },
   generator: new AngularComponentsGenerator({
     webComponentImportPath: () => 'components-library' // @todo support file paths (original file path of the class)
---
+++ examples/library-angular-components/projects/library-angular-components/src/public-api.ts
@@ -0,0 +1,5 @@
+/*
+ * Public API Surface of library-angular-components
+ */
+
+export * from './lib/generated-components/components/TagLitJs.component';
