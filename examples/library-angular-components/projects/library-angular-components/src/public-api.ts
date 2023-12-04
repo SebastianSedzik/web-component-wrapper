a67a268a2dfc659a7eb952a2b75537f6100482be
Sebastian Sędzik <s.sedzik@smartrecruiters.com>
2023-12-04 16:37:29 +0100

    chore(examples): cleanup

--- examples/library-angular-components/projects/library-angular-components/src/public-api.ts
+++ examples/library-angular-components/projects/library-angular-components/src/public-api.ts
@@ -2,6 +2,4 @@
  * Public API Surface of library-angular-components
  */
 
-// export * from './lib/generated-components/TagLitTs/src/public-api';
-// export * from './lib/generated-components/TagLitJs/src/public-api';
 export {}
